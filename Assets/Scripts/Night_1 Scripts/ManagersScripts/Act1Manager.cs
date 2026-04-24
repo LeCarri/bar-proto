450e20079ed5fe9070f6aac3064759f0e5e0b3ab
Olivia Stonehead <150560894+OliviaStonehead@users.noreply.github.com>
2026-04-24 16:39:57 -0300

    Paranoia UI
    Añadido el sistema de paranoia visualmente.

--- Assets/Scripts/Night_1 Scripts/ManagersScripts/Act1Manager.cs
+++ Assets/Scripts/Night_1 Scripts/ManagersScripts/Act1Manager.cs
@@ -126,6 +126,7 @@ public class Act1Manager : MonoBehaviour
     if (effectoParpadeo != null)
     {
         effectoParpadeo.IniciarParpadeo();
+        
         yield return new WaitForSeconds(1f);
         CambiarIluminacion("Servicio");
     }
@@ -143,7 +144,9 @@ public class Act1Manager : MonoBehaviour
     estadoActual = ActoState.Servicio;
     MostrarDialogo("Clientes ?... a trabajar.");
 
-     if (ambientBar != null && !ambientBar.isPlaying)
+        ParanoiaSystem.Instance.AddParanoia(25f);//sube la paranoia en un 25%
+
+        if (ambientBar != null && !ambientBar.isPlaying)
         {
             ambientBar.Play();
         }
