9c219ada56d84a52c2fcdf4bbd560473dc4497a8
valenti104 <valucho10@gmail.com>
2026-05-31 15:30:21 -0300

    estoy cansado jefe

--- Assets/Scripts/Night_3 Scripts/SimpleInteract.cs
+++ Assets/Scripts/Night_3 Scripts/SimpleInteract.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
-using UnityEngine;
 
 public class SimpleInteract : MonoBehaviour
 {
     public ClienteAct3 clienteReal;
+    public ClienteSotano clienteSotano;
 
     public void Interact()
     {
         if (clienteReal != null)
         {
             clienteReal.Interact();
+            return;
+        }
+
+        if (clienteSotano != null)
+        {
+            clienteSotano.Interact();
+            return;
         }
     }
 }
